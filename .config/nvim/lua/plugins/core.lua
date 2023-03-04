879b8bc5545472f4afc94931fd07c882be2e9c83
darklyspaced <srohanjd@gmail.com>
2023-03-04 11:21:01 +0800

    stable nvim

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -4,75 +4,75 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 return {
-  -- colourscheme setup
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" },
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
+    -- colourscheme setup
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = { style = "night" },
     },
-  },
 
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    enabled = false,
-  },
-
-  {
-    "echasnovski/mini.indentscope",
-    enabled = false,
-  },
-
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
+        },
     },
-    event = "VeryLazy",
-    opts = function()
-      return {
-        require("nvim-tree").setup({
-          sort_by = "case_sensitive",
-          view = {
-            adaptive_size = true,
-            mappings = {
-              list = {
-                { key = "u", action = "dir_up" },
-              },
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        enabled = false,
+    },
+
+    {
+        "echasnovski/mini.indentscope",
+        enabled = false,
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        event = "VeryLazy",
+        opts = function()
+            return {
+                require("nvim-tree").setup({
+                    sort_by = "case_sensitive",
+                    view = {
+                        adaptive_size = true,
+                        mappings = {
+                            list = {
+                                { key = "u", action = "dir_up" },
+                            },
+                        },
+                    },
+                    renderer = {
+                        group_empty = true,
+                    },
+                    filters = {
+                        dotfiles = true,
+                    },
+                }),
+            }
+        end,
+    },
+
+    {
+        "christoomey/vim-tmux-navigator",
+        event = "VeryLazy",
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        ---@class PluginLspOpts
+        opts = {
+            ---@type lspconfig.options
+            servers = {
+                clangd = {},
+                pyright = {},
             },
-          },
-          renderer = {
-            group_empty = true,
-          },
-          filters = {
-            dotfiles = true,
-          },
-        }),
-      }
-    end,
-  },
-
-  {
-    "christoomey/vim-tmux-navigator",
-    event = "VeryLazy",
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        clangd = {},
-        pyright = {},
-      },
+        },
     },
-  },
 
-  { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
 }
