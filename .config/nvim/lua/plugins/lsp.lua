e1c61e591066544d74c03f357af18d4f204c02c3
darklyspaced <srohanjd@gmail.com>
2023-10-01 08:49:36 +0800

    changed config a lil bit

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,174 +1,174 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			{ "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-			{ "folke/neodev.nvim", opts = {} },
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"williamboman/mason.nvim",
-			"simrat39/rust-tools.nvim",
-		},
-		opts = {
-			ensure_installed = {
-				"rust_analyzer",
-				"hls",
-				"lua_ls",
-				"jdtls",
-			},
-		},
-		config = function(_, opts)
-			require("mason-lspconfig").setup(opts)
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+            { "folke/neodev.nvim",  opts = {} },
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "williamboman/mason.nvim",
+            "simrat39/rust-tools.nvim",
+        },
+        opts = {
+            ensure_installed = {
+                "rust_analyzer",
+                "hls",
+                "lua_ls",
+                "jdtls",
+            },
+        },
+        config = function(_, opts)
+            require("mason-lspconfig").setup(opts)
 
-			local lspconfig = require("lspconfig")
-			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local lspconfig = require("lspconfig")
+            local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilites = lsp_capabilities,
-					})
-				end,
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilites = lsp_capabilities,
+                    })
+                end,
 
-				-- override rust-analyzer setup to delegate it to rust-tools
-				["rust_analyzer"] = function()
-					require("rust-tools").setup({})
-				end,
-			})
-		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp",
-		event = "InsertEnter",
-		dependencies = {
-			{ "L3MON4D3/LuaSnip" },
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		event = "InsertEnter",
-		dependencies = {
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
+                -- override rust-analyzer setup to delegate it to rust-tools
+                ["rust_analyzer"] = function()
+                    require("rust-tools").setup({})
+                end,
+            })
+        end,
+    },
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        event = "InsertEnter",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" },
+        },
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0
+                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
-			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = {
-					["<CR>"] = cmp.mapping.confirm({ select = false }),
-					["<Down>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
-						elseif has_words_before() then
-							cmp.complete()
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = {
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                    ["<Down>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
 
-					["<Up>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				},
-				preselect = "item",
-				completion = {
-					completeopt = "menu,menuone,noinsert",
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				experimental = {
-					ghost_text = {
-						hl_group = "CmpGhostText",
-					},
-				},
-				formatting = {
-					fields = { cmp.ItemField.Abbr, cmp.ItemField.Kind },
-					-- its always [LSP] smh
-					format = function(_, item)
-						item.menu = nil
-						return item
-					end,
-				},
-				sources = cmp.config.sources({
-					-- don't suggest text and snippets cause its just clutter
-					{
-						name = "nvim_lsp",
-						entry_filter = function(entry, _)
-							return (require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind())
-								and (require("cmp").lsp.CompletionItemKind.Snippet ~= entry:get_kind())
-						end,
-					},
-					{
-						name = "path",
-					},
-					{ name = "crates" },
-				}),
-			})
-		end,
-	},
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
+                    ["<Up>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                },
+                preselect = "item",
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                experimental = {
+                    ghost_text = {
+                        hl_group = "CmpGhostText",
+                    },
+                },
+                formatting = {
+                    fields = { cmp.ItemField.Abbr, cmp.ItemField.Kind },
+                    -- its always [LSP] smh
+                    format = function(_, item)
+                        item.menu = nil
+                        return item
+                    end,
+                },
+                sources = cmp.config.sources({
+                    -- don't suggest text and snippets cause its just clutter
+                    {
+                        name = "nvim_lsp",
+                        entry_filter = function(entry, _)
+                            return (require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind())
+                                and (require("cmp").lsp.CompletionItemKind.Snippet ~= entry:get_kind())
+                        end,
+                    },
+                    {
+                        name = "path",
+                    },
+                    { name = "crates" },
+                }),
+            })
+        end,
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
 
-			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			require("null-ls").setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.rustfmt,
-				},
-				on_attach = function(client, bufnr)
-					if client.supports_method("textDocument/formatting") then
-						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							group = augroup,
-							buffer = bufnr,
-							callback = function()
-								vim.lsp.buf.format({ async = false })
-							end,
-						})
-					end
-				end,
-			})
-		end,
-	},
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            require("null-ls").setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.rustfmt,
+                },
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                vim.lsp.buf.format({ async = false })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
+    },
 }
